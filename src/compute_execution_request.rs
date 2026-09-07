f72527c9072b2ced9c741db42d21eeb7460ee287
Leo <cheerleaderleo@outlook.com>
2026-09-07 18:09:22 -0400

    Export Blender burst planning modules

--- src/compute_execution_request.rs
+++ src/compute_execution_request.rs
@@ -8,7 +8,9 @@ mod request;
 pub use request::{COMPUTE_EXECUTION_REQUEST_SCHEMA_VERSION, ComputeExecutionRequest};
 
 pub mod accelerator_burst;
+pub mod blender_burst_work_plan;
 pub mod blender_content_store;
+pub mod blender_remote_inventory_document;
 pub mod blender_snapshot;
 pub mod blender_snapshot_document;
 pub mod capacity_admission;
